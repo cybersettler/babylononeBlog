ee420a358becc557fa9f9d392ef5c1c728b418af
Javier escobar <cybersettler@babylonone.com>
2016-09-07 16:29:37 +0200

    fixed announcement

--- data/Announcements.js
+++ data/Announcements.js
@@ -3,10 +3,10 @@ var Announcements = [
     title: "Upcoming event: Citizens' Debate on Space for Europe",
     content: '<p>The event will take place on Sepetember 10 ' +
     'throughout the European Space Agency member countries. ' +
-    'European citizens that went through a selection process' +
-    'will discuss about space matters in different sessions.' +
+    'European citizens that went through a selection process ' +
+    'will discuss about space matters in different sessions. ' +
     '<a target="_blank" href="http://citizensdebate.space/">' +
-    "Citizens' Debate</a> is part of an effort from ESA to " +
+    "Citizens' Debate on Space</a> is part of an effort from ESA to " +
     'involve european citizens in the space industry.' +
     '</p>',
     publishDate: "07.09.2016",
