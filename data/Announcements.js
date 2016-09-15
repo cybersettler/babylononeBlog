f3bf40c48339f701272f881b7e402c08560ca942
Javier escobar <cybersettler@babylonone.com>
2016-09-15 22:55:51 +0200

    added A Simple Response Announcement

--- data/Announcements.js
+++ data/Announcements.js
@@ -1,18 +1,15 @@
 var Announcements = [
   {
-    title: "Ongoing event: EO Open Science 2016",
-    content: '<p>The European Space Agency (ESA) is organizing the ' +
-    '2nd consultation meeting of EO Open Science in ESRIN (Frascati, Italy) ' +
-    'on 12–14 September 2016 to explore new challenges and opportunities ' +
-    'for EO research created by the rapid advances in Information and ' +
-    'Communications Technologies (ICT).</p>' +
+    title: "A Simple Response to an Elemental Message",
+    content: '<p>Final call for submissions to #ASimpleResponse ' +
+    'interstellar transmission; 16 Sept 23:59 UTC.</p>' +
     '<p>' +
-    '<a target="_blank" href="http://eoopenscience.esa.int/" class="btn btn-primary btn-lg active" role="button">Learn more</a>' +
+    '<a target="_blank" href="http://www.asimpleresponse.org/" class="btn btn-primary btn-lg active" role="button">Learn more</a>' +
     '</p>',
-    publishDate: "12.09.2016",
+    publishDate: "15.09.2016",
     author: "cybersettler@babylonone.com",
-    featureImageUri: "esa-logo.png",
-    featureImageAltText: "ESA logo"
+    featureImageUri: "simple_response_logo.png",
+    featureImageAltText: "A simple response logo"
   }
 ];
 
